cd623fe58d42a94fd208af68f96a3d34b13b3d06
Victor Mayer <victor.mayer@outlook.com>
2021-06-18 20:08:07 -0300

    CRUD customers implemented

--- server.js
+++ server.js
@@ -41,25 +41,33 @@ server.post('/categories', async (req, res) => {
     } catch {
         res.sendStatus(401)
     }
-})
+});
 
 server.get('/games', async (req, res) => {
     const { name } = req.query;
-    console.log(name);
     try{
-        const queryInput = `WHERE games.name ILIKE $1`
-        const query = await connection.query(`
-            SELECT games.*, categories.name AS "categoryName"
-            FROM games JOIN categories
-            ON games."categoryId" = categories.id 
-            ${queryInput}
-        `,[name+'%']);
-        res.send(name ? query.rows[0] : query.rows);
+        if(name){
+            const queryInput = `WHERE games.name ILIKE $1`
+            const query = await connection.query(`
+                SELECT games.*, categories.name AS "categoryName"
+                FROM games JOIN categories
+                ON games."categoryId" = categories.id 
+                ${queryInput}
+            `,[name+'%']);
+            res.send(query.rows[0]);
+        } else {
+            const query = await connection.query(`
+                SELECT games.*, categories.name AS "categoryName"
+                FROM games JOIN categories
+                ON games."categoryId" = categories.id
+            `);
+            res.send(query.rows);
+        }
     } catch(e) {
         console.log(e);
         res.sendStatus(400);
     }
-})
+});
 
 server.post('/games', async (req, res) => {
     const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
@@ -67,53 +75,157 @@ server.post('/games', async (req, res) => {
         const validated = await gameValidation(name, stockTotal, pricePerDay, categoryId);
         console.log(validated);
         if(validated.flag){
+            await connection.query('INSERT INTO games (name, image, stockTotal, categoryId, pricePerDay) VALUES ($1, $2, $3, $4, $5)',[name, image, stockTotal, categoryId, pricePerDay]);
             res.sendStatus(201);
         }else{
-            res.sendStatus(validated.status)
+            res.sendStatus(validated.status);
         }
     } catch(e){
-        console.log("entrei no catch, o erro é o seguinte:")
         console.log(e);
-        res.send(400);
+        res.sendStatus(400);
     }
-})
+});
 
-server.listen(4000, ()=>{
-    console.log("Server listening to port 4000.");
-})
+server.get('/customers', async (req, res) => {
+    const { cpf } = req.query;
+    try{
+        if(cpf){
+            const query = await connection.query(`SELECT * FROM customers WHERE cpf ILIKE $1`,[cpf+'%']);
+            res.send(query.rows);
+        } else {
+            const query = await connection.query(`SELECT * FROM customers`);
+            res.send(query.rows)
+        }
+    } catch(e){
+        console.log(e);
+        res.sendStatus(400);
+    }
+});
 
-async function gameValidation(name, stock, price, categoryId){
-    console.log("entrou na validação");
+server.get('/customers/:id', async (req, res) => {
+    const { id } = req.params;
+    try{
+        if (!isNaN(id)){
+            const query = await connection.query(`SELECT * FROM customers WHERE id = $1`,[id]);
+            res.send(query.rows[0]);
+        } else {
+            res.sendStatus(400);
+        }
+    } catch(e){
+        console.log(e);
+        res.sendStatus(400);
+    }
+});
+
+server.post('/customers', async (req, res) => {
+    const { name, phone, cpf, birthday } = req.body;
+    try{
+        const validated = await customerValidation(name, phone, cpf, birthday);
+        console.log(validated);
+        if(validated.flag){
+            await connection.query(`INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)`, [name, phone, cpf, birthday]);
+            res.sendStatus(201);
+        } else {
+            res.sendStatus(validated.status);
+        }
+    } catch(e) {
+        console.log("caiu no catch!");
+        console.log(e);
+        res.sendStatus(400);
+    }
+});
+
+server.put('/customers/:id', async (req, res) => {
+    const { name, phone, cpf, birthday } = req.body;
+    const { id } = req.params;
+    try{
+        if(isNaN(id)){
+            res.sendStatus(400);
+            return;
+        }
+        const validated = await customerValidation(name, phone, cpf, birthday);
+        console.log(validated);
+        if(validated.flag){
+            await connection.query(`
+                UPDATE customers
+                SET name = $1, phone = $2, cpf = $3, birthday = $4
+                WHERE id = $5`, [name, phone, cpf, birthday, id]);
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(validated.status);
+        }
+    } catch(e) {
+        console.log("caiu no catch!");
+        console.log(e);
+        res.sendStatus(400);
+    }
+});
+
+async function customerValidation(name, phone, cpf, birthday){
+    let result = {flag: true, status:201}
+    if(isNaN(parseInt(phone)) || isNaN(parseInt(cpf))){
+        console.log("phone or cpf isNaN");
+        result.flag=false;
+        result.status=400;
+        return result;
+    }
+    if(name.length === 0 || cpf.length !== 11 || phone.length < 10 || phone.length > 11){
+        console.log("lengths invalid");
+        result.flag=false;
+        result.status=400;
+        return result;
+    }
+    const regex = /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/;
+    if(!regex.test(birthday)){
+        console.log("data inválida");
+        result.flag=false;
+        result.status=400;
+        return result;
+    }
+    const customers = await connection.query(`SELECT * FROM customers`);
+    if(customers.rows.length > 0){
+        console.log("entra no if");
+        customers.rows.forEach(customer => {
+            console.log(customer);
+            if(customer.cpf === cpf){
+                console.log("cpf ja existe");
+                result.flag=false;
+                result.status=409;
+            }
+        });
+    }
+    return result;
+}
+
+async function gameValidation(name, stockTotal, pricePerDay, categoryId){
     let result = {flag: true, status:200};
     let canReturn=false;
     if (name.length === 0){
-        console.log("nome do jogo vazio");
         result.flag = false;
         result.status = 400;
         return result;
     }
-    if (stock <= 0 || price <= 0){
-        console.log("preço ou estoque menor ou igual a 0");
+    if (stockTotal <= 0 || pricePerDay <= 0){
         result.flag = false;
         result.status = 400;
         return result;
     }
-    const query = await connection.query('SELECT * FROM games');
-        query.rows.forEach(game => {
+    const games = await connection.query('SELECT * FROM games');
+    if(games.rows.length > 0){
+        games.rows.forEach(game => {
             if(game.name === name){
-                console.log("nome de jogo repetido");
                 result.flag = false;
                 result.status = 409;
                 canReturn = true;
             }
         })
+    }
     if(canReturn)return result;
     result.flag = false;
     result.status = 400;
-    query = await connection.query('SELECT * FROM categories');
-    query.rows.forEach(category => {
+    const categories = await connection.query('SELECT * FROM categories');
+    categories.rows.forEach(category => {
         if(category.id === categoryId){
-            console.log("categoria existente");
             result.flag = true;
             result.status = 200;
         }
@@ -128,13 +240,18 @@ async function categoryValidation(name){
         result.status = 400;
     } else {
         const query = await connection.query('SELECT * FROM categories');
-        query.rows.forEach(category => {
-            if(category.name === name){
-                result.flag = false;
-                result.status = 409;
-            }
-        })
+        if(query.rows.length > 0){
+            query.rows.forEach(category => {
+                if(category.name === name){
+                    result.flag = false;
+                    result.status = 409;
+                }
+            })
+        }
     }
     return result;
 }
 
+server.listen(4000, ()=>{
+    console.log("Server listening to port 4000.");
+});
